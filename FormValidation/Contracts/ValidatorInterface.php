0ad62c04a25352f0de5c5d0595acbd8cdfa50044
Rohmad Kadarwanto <rohmadkadarwanto@gmail.com>
2026-08-07 13:55:44 +0700

    Update file loader

--- FormValidation/Contracts/ValidatorInterface.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Kodhe\FormValidation\Contracts;
-
-interface ValidatorInterface
-{
-    public function validate($value, $params = []);
-    public function getMessage(): string;
-}
